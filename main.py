90c24494a1fd991f18cea45c77817cf5a1dacd85
nmra-abarthmaron <asa.barthmaron@neumoratx.com>
2022-08-16 20:57:47 +0000

    Create max projections, and run first cp analyses on aws. Code already needs refactor

--- main.py
+++ main.py
@@ -6,26 +6,30 @@ Primary entrypoint for running the data-processing pipeline.
 import pathlib
 import skimage.io
 import matplotlib.pyplot as plt
+import os
 
 # Path for raw image data (z-stack tifs from confocal)
 data_path = pathlib.Path(
-    '/shared-data/research/cell-imaging/220811 96w 9 Gene KO /tifs'
+    '/fsx/raw-data/220810 96w Dye Chem Controls/tifs'
+)
+# Path for saving max projections
+save_dir = pathlib.Path(
+    '/fsx/processed-data/220810 96w Dye Chem Controls/max_projections'
 )
 
 # Check if max projections have already been created
-if not data_path.exists():
-    data_path.mkdir()
+if not save_dir.exists():
+    save_dir.mkdir(parents=True)
 
-    # Create max projections
-    for tif in data_path.glob('*.tif'):
+# Create max projections
+for tif in data_path.glob('*.tif'):
 
-        # Read image (z-stack), pil plugin required bc of ome-tif xml data
-        img = skimage.io.imread(tif, plugin='pil')
+    # Read image (z-stack), pil plugin required bc of ome-tif xml data
+    img = skimage.io.imread(tif, plugin='pil')
 
-        # Create max projection
-        max_projection = img.max(axis=0)
+    # Create max projection
+    max_projection = img.max(axis=0)
 
-        # Save max projection in data_path
-        fname = tif.name
-        save_dir = data_path.parent / 'max_projections'
-        skimage.io.imsave(save_dir / fname, max_projection)
+    # Save max projection in data_path
+    fname = tif.name
+    skimage.io.imsave(save_dir / fname, max_projection)
--- prototyping.py
+++ prototyping.py
@@ -1,16 +1,59 @@
 import cellprofiler_core.pipeline
 import cellprofiler_core.preferences
 import cellprofiler_core.utilities.java
+import cellprofiler
 import pathlib
-
     
-if __name__ == "__main__":
-    
-    # Replacement config class, to circumvent the default wx-based GUI class
-    cellprofiler_core.preferences.set_headless()
+#if __name__ == "__main__":
 
-    # Start java VM
-    cellprofiler_core.utilities.java.start_java()
+# Experiment name
+exp_name = '220810 96w Dye Chem Controls'
+# Analysis run name & date
+analysis_date = '2022-08-16'
+analysis_name = 'neurite_segment'
 
-    # Load pipeline, for this prototype we'll use 'soma_image'
-    pipeline = cellprofiler_core.pipeline.Pipeline()
+# Replacement config class, to circumvent the default wx-based GUI class
+cellprofiler_core.preferences.set_headless()
+
+# Start java VM
+cellprofiler_core.utilities.java.start_java()
+
+# Load pipeline, for this prototype we'll use 'soma_image'
+pipeline = cellprofiler_core.pipeline.Pipeline()
+
+# Open a pipeline
+pipeline = cellprofiler_core.pipeline.Pipeline()
+pipeline.load(
+    '/home/ubuntu/cell-imaging/cellprofiler_pipelines/2022-08-16_soma_image.cppipe'
+)
+
+# Set default output directory
+save_dir = pathlib.Path('/fsx/processed-data') / exp_name / analysis_date / analysis_name
+
+# Create save dir if it doesn't exist
+if not save_dir.exists():
+    save_dir.mkdir(parents=True)
+
+cellprofiler_core.preferences.set_default_output_directory(save_dir)
+
+# Set image file list
+data_dir = pathlib.Path('/fsx/processed-data') / exp_name / 'max_projections'
+
+file_list = list(data_dir.glob('*.tif'))
+files = [file.as_uri() for file in file_list]
+pipeline.read_file_list(files)
+
+# Change the name of the spreadsheet export
+pipeline.modules()[11].settings()[16].set_value(
+    analysis_date + '_' + 
+    analysis_name + '_'
+)
+
+# Run the pipeline
+output_measurements = pipeline.run()
+
+# Stop java VM
+cellprofiler_core.utilities.java.stop_java()
+
+
+print(analysis_name + ' analysis complete')
