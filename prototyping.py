5b0b6a2fecd08beddfb836834e4f274f0ac98b60
nmra-abarthmaron <asa.barthmaron@neumoratx.com>
2022-08-22 15:29:12 +0000

    Work on confocal imaging analysis notebook, minor changes to pipeline

--- prototyping.py
+++ prototyping.py
@@ -7,10 +7,19 @@ import pathlib
 #if __name__ == "__main__":
 
 # Experiment name
-exp_name = '220810 96w Dye Chem Controls'
+exp_name = '220811 96w 9 Gene KO '
+#exp_name = '220810 96w Dye Chem Controls/'
+
 # Analysis run name & date
 analysis_date = '2022-08-16'
-analysis_name = 'neurite_segment'
+#analysis_name = 'soma_image'
+analysis_name = 'soma_objects'
+#analysis_name = 'neurite_segment'
+
+# CP pipeline file
+#pipeline_file = '/home/ubuntu/cell-imaging/cellprofiler_pipelines/2022-08-16_soma_image.cppipe'
+pipeline_file = '/home/ubuntu/cell-imaging/cellprofiler_pipelines/2022-08-16_soma_objects.cppipe'
+#pipeline_file = '/home/ubuntu/cell-imaging/cellprofiler_pipelines/2022-08-16_neurite_segment.cppipe'
 
 # Replacement config class, to circumvent the default wx-based GUI class
 cellprofiler_core.preferences.set_headless()
@@ -23,9 +32,7 @@ pipeline = cellprofiler_core.pipeline.Pipeline()
 
 # Open a pipeline
 pipeline = cellprofiler_core.pipeline.Pipeline()
-pipeline.load(
-    '/home/ubuntu/cell-imaging/cellprofiler_pipelines/2022-08-16_soma_image.cppipe'
-)
+pipeline.load(pipeline_file)
 
 # Set default output directory
 save_dir = pathlib.Path('/fsx/processed-data') / exp_name / analysis_date / analysis_name
@@ -44,10 +51,14 @@ files = [file.as_uri() for file in file_list]
 pipeline.read_file_list(files)
 
 # Change the name of the spreadsheet export
-pipeline.modules()[11].settings()[16].set_value(
+pipeline.modules()[10].settings()[16].set_value(
     analysis_date + '_' + 
     analysis_name + '_'
 )
+
+# Applicable to soma objects analysis only. should be done in cppipe file
+pipeline.modules()[10].settings()[4].set_value('yes')
+pipeline.modules()[10].settings()[5].set_value('yes')
 
 # Run the pipeline
 output_measurements = pipeline.run()
