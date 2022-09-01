94a9f6b56f75932b1688db7315c6f9c0e93ab93a
nmra-abarthmaron <asa.barthmaron@neumoratx.com>
2022-09-01 20:43:51 +0000

    first work on boxplot app

--- gui/boxplot_app.py
+++ gui/boxplot_app.py
@@ -1,82 +1,88 @@
 from dash import Dash, dcc, html, Input, Output, State, ctx
+from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import plotly.express as px
 import skimage.io as sio
 import pathlib
 import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.matplotlylib as pplt
+import numpy.matlib as np
 
 app = Dash(__name__)
 
-img_path = ('/fsx/processed-data/220811 96w 9 Gene KO /2022-08-22_soma_objects/soma_outlines/Plate 2 20x SD_B08_561 SD.tiff')
+# Load processed cellprofiler data from csv
+data_path = '/fsx/processed-data/220811 96w 9 Gene KO /2022-08-22_soma_objects/2022-08-22_soma_objects_Image.csv'
+data = pd.read_csv(data_path)
+
+# Set index to well name
+data.index = data['FileName_TMRM']
+
+# Remove unwanted columns
+drop_columns = pd.read_csv('/fsx/processed-data/220811 96w 9 Gene KO /2022-08-22_soma_objects/2022-08-30_soma_objects_image_column_drop_list.csv', header=None, dtype=str)
+drop_columns = np.array(drop_columns).astype(str).flatten()
+for col in drop_columns:
+    data = data.drop(data.columns[data.columns.str.contains(col)], axis=1)
+
+# Load platemap / well conditions
+pm = pd.read_csv('/fsx/processed-data/220811 96w 9 Gene KO /2022-08-22_soma_objects/soma_outlines/220811_well_conditions.csv', index_col='filename')# Add condition labels to well dataframe
+# data['conditions'] = pm.reindex(data.index)
+data = data.reindex(pm.index)
+
 app.layout = html.Div([
-    dcc.Dropdown(value=img_path, id='image-dropdown'),
+
+    html.Div([
+
+        dcc.Dropdown(
+            value='Mean_soma_Intensity_MedianIntensity_CellROX',
+            options=data.columns, id='measurement-dropdown'
+        ),
+    ], style={'padding': 10, 'flex': 1, 'max-width': 400}),
+
     dcc.Graph(id='test')
 ])
 
 @app.callback(
     Output(component_id='test', component_property='figure'),
-    Input(component_id='image-dropdown', component_property='value'),
+    Input(component_id='measurement-dropdown', component_property='value'),
 )
-def update_image(img_path):
-    img = sio.imread(img_path)
-    # fig = plt.imshow(img)
-    fig = px.imshow(img)
-    renderer = pplt.PlotlyRenderer(fig)
-    exporter = pplt.Exporter(renderer)
-    exporter.run(fig)
-    # plt.show()
+def select_plot_type(measurement):
+
+    # Check if measurement is multi-channel or single-channel
+    channel_names = (' LysoSensor', 'CellROX', 'TMRM', 'Syto')
+    multi_channels = any([x in measurement for x in channel_names])
+
+    # Return plotting fn
+    if multi_channels:
+        return update_multi_ch_fig(measurement)
+    else:
+        return update_single_ch_fig(measurement)
+
+
+def update_multi_ch_fig(measurement):
+    html.Br(),
+    fig = make_subplots(rows=2, cols=2)
+
+    fig.add_trace(
+        go.Box(x=pm['condition'], y=data[measurement]), row=1, col=1,
+    )
+    fig.add_trace(
+        go.Box(x=pm['condition'], y=data[measurement]), row=1, col=2,
+    )
+    fig.add_trace(
+        go.Box(x=pm['condition'], y=data[measurement]), row=2, col=1,
+    )
+    fig.add_trace(
+        go.Box(x=pm['condition'], y=data[measurement]), row=2, col=2,
+    )
+
+    fig.update_layout(
+        width=1280,
+        height=768
+    )
     return fig
-app.layout = html.Div([
 
-    html.Div([
-            html.Br(),
-            html.Label('Experiment:'),
-            dcc.Dropdown(
-                exps,
-                '2022-08-22_soma_objects',
-                id='experiment-dropdown'
-            ),
-
-            html.Br(),
-            html.Label('Condition:'),
-            dcc.Dropdown(value=img_df.index[0], id='condition-dropdown'),
-
-            html.Br(),
-            html.Label('Image:'),
-            dcc.Dropdown(id='image-dropdown'),
-
-            html.Br(),
-            html.Button(id='prev-button', n_clicks=0, children='Prev image'),
-            html.Button(id='next-button', n_clicks=0, children='Next image'),
-
-            html.Br(),
-            html.Br(),
-            html.Label('Overlay soma outlines?'),
-            dcc.RadioItems(
-                id='overlay-outlines', 
-                # options={True : 'Yes', False : 'No'},
-                options=['Yes', 'No'],
-                value = 'Yes'
-                ),
-
-            html.Br(),
-            html.Label('Set max. intensity:'),
-            dcc.Slider(0, 1,
-                    value=1,
-                    marks = {0: 'min', 1 : 'max'},
-                    id='img-max-slider'
-            ),            
-            
-        ], 
-        style={'padding': 10, 'flex': 1, 'max-width': 300}
-    ),
-    
-    dcc.Graph(id='soma-outlines')
-
-], style={'display': 'flex', 'flex-direction': 'row'})
-
+def update_single_ch_fig(measurement):
+    fig = px.box(x=pm['condition'], y=data[measurement],)
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
