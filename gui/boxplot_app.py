d8fe4a7db7df598a92f5786abcfa0d5cc10e7b2f
nmra-abarthmaron <asa.barthmaron@neumoratx.com>
2022-09-01 17:50:16 +0000

    minor fixes

--- gui/boxplot_app.py
+++ gui/boxplot_app.py
@@ -28,6 +28,55 @@ def update_image(img_path):
     exporter.run(fig)
     # plt.show()
     return fig
+app.layout = html.Div([
+
+    html.Div([
+            html.Br(),
+            html.Label('Experiment:'),
+            dcc.Dropdown(
+                exps,
+                '2022-08-22_soma_objects',
+                id='experiment-dropdown'
+            ),
+
+            html.Br(),
+            html.Label('Condition:'),
+            dcc.Dropdown(value=img_df.index[0], id='condition-dropdown'),
+
+            html.Br(),
+            html.Label('Image:'),
+            dcc.Dropdown(id='image-dropdown'),
+
+            html.Br(),
+            html.Button(id='prev-button', n_clicks=0, children='Prev image'),
+            html.Button(id='next-button', n_clicks=0, children='Next image'),
+
+            html.Br(),
+            html.Br(),
+            html.Label('Overlay soma outlines?'),
+            dcc.RadioItems(
+                id='overlay-outlines', 
+                # options={True : 'Yes', False : 'No'},
+                options=['Yes', 'No'],
+                value = 'Yes'
+                ),
+
+            html.Br(),
+            html.Label('Set max. intensity:'),
+            dcc.Slider(0, 1,
+                    value=1,
+                    marks = {0: 'min', 1 : 'max'},
+                    id='img-max-slider'
+            ),            
+            
+        ], 
+        style={'padding': 10, 'flex': 1, 'max-width': 300}
+    ),
+    
+    dcc.Graph(id='soma-outlines')
+
+], style={'display': 'flex', 'flex-direction': 'row'})
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
