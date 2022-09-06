41ab9152421fd1be4e5f5cfa2f05820ced877359
nmra-abarthmaron <asa.barthmaron@neumoratx.com>
2022-09-06 19:22:25 +0000

    integrated pipeline app - image inspection, image boxplots, and soma boxplots

---
+++ gui/pages/image.py
@@ -0,0 +1,157 @@
+from dash import callback, dcc, html, Input, Output, State, ctx
+import dash
+import plotly.graph_objects as go
+import skimage.io as sio
+import pathlib
+import pandas as pd
+
+dash.register_page(__name__)
+
+# img = sio.imread('/fsx/processed-data/220811 96w 9 Gene KO /2022-08-22_soma_objects/soma_outlines/Plate 2 20x SD_B08_561 SD.tiff')
+exp_path = pathlib.Path('/fsx/processed-data/220811 96w 9 Gene KO /')
+exps = [x.name for x in exp_path.iterdir() if x.is_dir()]
+img_path = pathlib.Path(
+    '/fsx/processed-data/220811 96w 9 Gene KO /2022-08-22_soma_objects/soma_outlines'
+)
+img_df = pd.read_csv(img_path / '220811_well_conditions.csv', index_col='condition')
+
+layout = html.Div([
+
+    html.Div([
+            html.Br(),
+            html.Label('Experiment:'),
+            dcc.Dropdown(
+                exps,
+                '2022-08-22_soma_objects',
+                id='experiment-dropdown'
+            ),
+
+            html.Br(),
+            html.Label('Condition:'),
+            dcc.Dropdown(value=img_df.index[0], id='condition-dropdown'),
+
+            html.Br(),
+            html.Label('Image:'),
+            dcc.Dropdown(id='image-dropdown'),
+
+            html.Br(),
+            html.Button(id='prev-button', n_clicks=0, children='Prev image'),
+            html.Button(id='next-button', n_clicks=0, children='Next image'),
+
+            html.Br(),
+            html.Br(),
+            html.Label('Overlay soma outlines?'),
+            dcc.RadioItems(
+                id='overlay-outlines', 
+                # options={True : 'Yes', False : 'No'},
+                options=['Yes', 'No'],
+                value = 'Yes'
+                ),
+
+            html.Br(),
+            html.Label('Set max. intensity:'),
+            dcc.Slider(0, 1,
+                    value=1,
+                    marks = {0: 'min', 1 : 'max'},
+                    id='img-max-slider'
+            ),            
+            
+        ], 
+        style={'padding': 10, 'flex': 1, 'max-width': 300}
+    ),
+    
+    dcc.Graph(id='soma-outlines')
+
+], style={'display': 'flex', 'flex-direction': 'row'})
+
+# Set condition name options in image dropdown, given an experiment
+@callback(
+    Output('condition-dropdown', 'options'),
+    Input('experiment-dropdown', 'value')
+)
+def set_image_options(condition):
+    return img_df.index.unique()
+
+# Set image name options in image dropdown, given a well condition
+@callback(
+    Output('image-dropdown', 'options'),
+    Input('condition-dropdown', 'value')
+)
+def set_image_options(condition):
+    return img_df.loc[condition].filename
+
+# Set image name value in the image dropdown menu, contextually.
+# Contexts - default, next button, prev button
+@callback(
+    Output('image-dropdown', 'value'),
+    Input('next-button', 'n_clicks'),
+    Input('prev-button', 'n_clicks'),
+    Input('image-dropdown', 'options'),
+    State('image-dropdown', 'value'),
+    prevent_initial_call=True
+)
+def update_image_name(_0, _1, img_options, img_name):
+    triggered_id = ctx.triggered_id
+    if triggered_id == 'image-dropdown':
+        return set_default_img(img_options)
+    if triggered_id == 'next-button':
+        return next_image(img_options, img_name)
+    if triggered_id == 'prev-button':
+        return prev_image(img_options, img_name)
+
+def set_default_img(img_options):
+    return img_options[0]
+
+def next_image(img_options, img_name):
+    i_img = img_options.index(img_name)
+    i_img += 1
+    i_img = i_img % len(img_options)
+    return img_options[i_img]
+
+def prev_image(img_options, img_name):
+    i_img = img_options.index(img_name)
+    i_img -= 1
+    i_img = i_img % len(img_options)
+    return img_options[i_img]
+
+# Take image name, load images, and plot
+@callback(
+    Output(component_id='soma-outlines', component_property='figure'),
+    Input(component_id='image-dropdown', component_property='value'),
+    Input('overlay-outlines', 'value'),
+    Input('img-max-slider', 'value')
+    # Input(component_id='next-image-button', component_property='n_clicks')
+)
+def update_image(img_name, soma_outlines, max_intensity):
+
+    # Load outline file
+    # img_name = pd.read_json(img_name)
+    outline_name = img_name + 'f'   # because cp saves them as 'tiff' not 'tif'
+    outline = sio.imread(img_path / outline_name)
+    outline = outline.astype(object)
+    outline[outline == 0] = None
+
+    # This is kinda a hack, should be done with the cp image csv
+    ch_names = [405, 488, 561, 647]
+    img = sio.imread(img_path.parent.parent /'max_projections'/ img_name.replace('tif', 'tif'))
+
+    # Set max intensity
+    img[img > (img.max() * max_intensity)] = img.max() * max_intensity
+
+    fig = go.Figure()
+    fig.add_trace(go.Heatmap(z=img, colorscale=['#000000', '#FFFFFF'], 
+                             showscale=False))
+    if soma_outlines == 'Yes':
+        fig.add_trace(go.Heatmap(z=outline, colorscale=['#FFFFFF', '#00FF00'], showscale=False))
+    
+    # fig = px.imshow(img, color_continuous_scale=['#000000', '#FFFFFF'])
+    # fig.add_trace(px.imshow(img, color_continuous_scale=['#000000', '#00FF00']).data[0])
+    
+
+    fig.update_layout(
+        xaxis={'showticklabels': False},
+        yaxis={'showticklabels': False}, 
+        width=1024, height=1024
+    )
+
+    return fig
---
+++ gui/pages/soma_boxplot.py
@@ -0,0 +1,179 @@
+from dash import callback, dcc, html, Input, Output, State, ctx
+import dash
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import plotly.express as px
+import skimage.io as sio
+import pathlib
+import pandas as pd
+import numpy.matlib as np
+from itertools import cycle
+import seaborn as sns
+
+dash.register_page(__name__)
+
+# Load processed cellprofiler data from csv
+data_path = '/fsx/processed-data/220811 96w 9 Gene KO /2022-08-22_soma_objects/2022-08-22_soma_objects_Image.csv'
+data = pd.read_csv(data_path)
+
+# Set index to well name
+data.index = data['FileName_TMRM']
+
+# Remove unwanted columns
+drop_columns = pd.read_csv('/fsx/processed-data/220811 96w 9 Gene KO /2022-08-22_soma_objects/2022-08-30_soma_objects_image_column_drop_list.csv', header=None, dtype=str)
+drop_columns = np.array(drop_columns).astype(str).flatten()
+for col in drop_columns:
+    data = data.drop(data.columns[data.columns.str.contains(col)], axis=1)
+
+# Load platemap / well conditions
+pm = pd.read_csv('/fsx/processed-data/220811 96w 9 Gene KO /2022-08-22_soma_objects/soma_outlines/220811_well_conditions.csv', index_col='filename')# Add condition labels to well dataframe
+# data['conditions'] = pm.reindex(data.index)
+data = data.reindex(pm.index)
+
+# Reformat soma data
+data_path = '/fsx/processed-data/220811 96w 9 Gene KO /2022-08-22_soma_objects/2022-08-22_soma_objects_soma.csv'
+soma_data = pd.read_csv(data_path)
+pm_soma = pd.DataFrame()
+pm_soma['condition'] = pm.loc[soma_data['FileName_TMRM']]['condition']
+# pm_soma.columns = ['condition']
+drop_columns = pd.read_csv('/fsx/processed-data/220811 96w 9 Gene KO /2022-08-22_soma_objects/2022-08-30_soma_objects_soma_column_drop_list.csv', header=None, dtype=str)
+drop_columns = np.array(drop_columns).astype(str).flatten()
+
+for col in drop_columns:
+    data = soma_data.drop(
+        soma_data.columns[soma_data.columns.str.contains(col)], axis=1)
+pm = pm_soma
+
+data.index = pm['condition']
+pm.index = pm['condition']
+conditions = pm.index.unique().tolist()
+
+# neumora color palette
+orange = '#E89377'
+green = '#67C478'
+purple = '#AAAADD'
+blue = '#66CCDD'
+colorblind = ["#0173B2", "#DE8F05", "#029E73", "#D55E00", "#CC78BC",
+            "#CA9161", "#FBAFE4", "#949494", "#ECE133", "#56B4E9"]
+palette = sns.color_palette("husl", pm['condition'].unique().shape[0])
+
+layout = html.Div([
+
+    html.Div([
+
+        dcc.Dropdown(
+            value='Intensity_MeanIntensity_CellROX',
+            # value='Mean_soma_Intensity_MedianIntensity_CellROX',
+            # value='Mean_soma_AreaShape_Area',
+            options=data.columns, id='measurement-dropdown'
+        ),
+    ], style={'padding': 10, 'flex': 1, 'max-width': 400}),
+
+    dcc.Graph(id='soma-boxplots')
+])
+
+@callback(
+    Output(component_id='soma-boxplots', component_property='figure'),
+    Input(component_id='measurement-dropdown', component_property='value'),
+)
+def select_plot_type(measurement):
+
+    # Check if measurement is multi-channel or single-channel
+    channel_names = ('LysoSensor', 'CellROX', 'TMRM', 'Syto')
+    multi_channels = any([x in measurement for x in channel_names])
+
+    # Return plotting fn
+    if multi_channels:
+        return update_multi_ch_fig(measurement, channel_names)
+    else:
+        return update_single_ch_fig(measurement)
+
+
+def update_multi_ch_fig(measurement, ch_names):
+    html.Br(),
+    n_rows = 2
+    n_cols = 2
+    fig = make_subplots(rows=2, cols=2, subplot_titles=ch_names)
+
+    ch = ch_names[np.where([x in measurement for x in ch_names])[0][0]]
+
+    # Give it a better name
+
+    for i_ch in range(len(ch_names)):
+        
+        m = measurement.replace(ch, ch_names[i_ch])
+        subplot_inds = np.unravel_index(i_ch, [n_rows, n_cols])
+
+        for i_cond in range(len(conditions)):
+            bar_data = data[m].loc[conditions[i_cond]]
+            fig.add_trace(
+                go.Violin(
+                    y=bar_data, 
+                    # points='all', pointpos=0, jitter=0.5, 
+                    line={'color': '#444444', 'width': 1},
+                    marker={'color': '#666666', 'size': 3}, 
+                    # fillcolor= 'rgb' + str(palette[i_cond]),
+                    fillcolor= colorblind[i_cond % len(colorblind)]
+                ),
+                row=subplot_inds[0]+1, col=subplot_inds[1]+1
+            )
+
+    fig.update_layout(
+        width=1280,
+        height=768,
+        showlegend=False,
+        xaxis1 = dict(
+            tickmode = 'array',
+            tickvals = np.arange(len(conditions)),
+            ticktext = conditions
+        ),
+        xaxis2 = dict(
+            tickmode = 'array',
+            tickvals = np.arange(len(conditions)),
+            ticktext = conditions
+        ),
+        xaxis3 = dict(
+            tickmode = 'array',
+            tickvals = np.arange(len(conditions)),
+            ticktext = conditions
+        ),
+        xaxis4 = dict(
+            tickmode = 'array',
+            tickvals = np.arange(len(conditions)),
+            ticktext = conditions
+        )
+    )
+    fig.update_xaxes(showgrid=True)
+    fig.update_yaxes(showgrid=True)
+
+    return fig
+
+def update_single_ch_fig(measurement):
+    fig = go.Figure()
+    for i_cond in range(len(conditions)):
+        bar_data = data[measurement].loc[conditions[i_cond]]
+        fig.add_trace(
+            go.Violin(
+                y=bar_data,
+                # boxpoints='all', pointpos=0, jitter=0.5, 
+                line={'color': '#444444', 'width': 2},
+                marker={'color': '#555555', 'size': 6}, 
+                # fillcolor= 'rgb' + str(palette[i_cond]),
+                fillcolor=  colorblind[i_cond % len(colorblind)]
+
+            )
+        )
+    # fig = px.box(x=pm['condition'], y=data[measurement],)
+    fig.update_layout(
+        width=1024,
+        height=512,
+        showlegend=False,
+        xaxis = dict(
+            tickmode = 'array',
+            tickvals = np.arange(len(conditions)),
+            ticktext = conditions
+        )
+    )
+    fig.update_xaxes(showgrid=True)
+    fig.update_yaxes(showgrid=True)
+    return fig
