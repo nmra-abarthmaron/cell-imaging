ea33b732b6a2622b9c4110b33a4ffc2820053560
nmra-abarthmaron <asa.barthmaron@neumoratx.com>
2022-12-22 21:35:03 +0000

    minor cleanups to notebooks

--- process/run_cp_pipeline.sh
+++ process/run_cp_pipeline.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-input_dir="/lab/processed-data/221121_45_Gene_KO_Screen/max_projections"
+input_dir="/lab/raw-data/221214_EA_GenTONIK_SM/max_projections"
 # input_dir="/fsx/processed-data/220929 Mattek 20x SD MIP TIFs/max_projections"
-output_dir="/lab/processed-data/221121_45_Gene_KO_Screen/2022-12-15_soma_segment"
+output_dir="/lab/processed-data/221214_EA_GenTONIK_SM/2022-12-19_neurite_segment_Emin"
 plugins_dir="/home/ubuntu/CellProfiler-plugins"
-cppipe_path='/home/ubuntu/cell-imaging/cellprofiler_pipelines/2022-12-15_soma_segment.cppipe'
+cppipe_path='/home/ubuntu/cell-imaging/cellprofiler_pipelines/2022-12-19_neurite_segment_Emin.cppipe'
 
 #conda init /bin/bash
 #conda activate cell-imaging-2
