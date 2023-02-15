a7a727a4727088846c2f73d211869398954f9dae
nmra-abarthmaron <asa.barthmaron@neumoratx.com>
2023-02-15 15:55:06 -0500

    shell script path update

--- process/run_cp_pipeline.sh
+++ process/run_cp_pipeline.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-input_dir="/lab/processed-data/EXP_JSM002_NGN_DIV18_wholeplate/max_projections"
+input_dir="/lab/processed-data/EXP_JSM004_MN_DIV35/max_projections"
 # input_dir="/fsx/processed-data/220929 Mattek 20x SD MIP TIFs/max_projections"
-output_dir="/lab/processed-data/EXP_JSM002_NGN_DIV18_wholeplate/2023-01-09_tdp43_analysis"
+output_dir="/lab/processed-data/EXP_JSM004_MN_DIV35/2023-02-01_jessie_tdp-43_MN_hand_drawn"
 plugins_dir="/home/ubuntu/CellProfiler-plugins"
-cppipe_path='/home/ubuntu/cell-imaging/cellprofiler_pipelines/2023-01-09_jessie_tdp-43.cppipe'
+cppipe_path='/home/ubuntu/cell-imaging/cellprofiler_pipelines/2023-02-01_jessie_tdp-43_MN_hand_drawn.cppipe'
 
 #conda init /bin/bash
 #conda activate cell-imaging-2
