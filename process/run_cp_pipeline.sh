64e666216e87a0125ac28a143b55777e49ea1b52
nmra-abarthmaron <asa.barthmaron@neumoratx.com>
2023-02-17 10:40:52 -0500

    Cellpose run on old NGN2 experiment

--- process/run_cp_pipeline.sh
+++ process/run_cp_pipeline.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-input_dir="/lab/processed-data/EXP_JSM004_MN_DIV35/max_projections"
+input_dir="/lab/processed-data/EXP_JSM002_NGN2_DIV25_wholeplate/max_projections"
 # input_dir="/fsx/processed-data/220929 Mattek 20x SD MIP TIFs/max_projections"
-output_dir="/lab/processed-data/EXP_JSM004_MN_DIV35/2023-02-01_jessie_tdp-43_MN_hand_drawn"
+output_dir="/lab/processed-data/EXP_JSM002_NGN2_DIV25_wholeplate/2023-02-15_cellpose_tdp-43"
 plugins_dir="/home/ubuntu/CellProfiler-plugins"
-cppipe_path='/home/ubuntu/cell-imaging/cellprofiler_pipelines/2023-02-01_jessie_tdp-43_MN_hand_drawn.cppipe'
+cppipe_path='/home/ubuntu/cell-imaging/cellprofiler_pipelines/2023-02-15_cellpose_tdp-43.cppipe'
 
 #conda init /bin/bash
 #conda activate cell-imaging-2
