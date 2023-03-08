32a6cff79ae6ce453eba5aa71a7c6c38c46fff80
nmra-abarthmaron <asa.barthmaron@neumoratx.com>
2023-03-08 10:15:10 -0500

    TDP43 analysis, some live cell analysis

--- process/run_cp_pipeline.sh
+++ process/run_cp_pipeline.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-input_dir="/lab/processed-data/EXP_JSM002_NGN2_DIV25_wholeplate/max_projections"
+input_dir="/lab/raw-data/230227_iN_B3_DIV28_LiveCell/1__2023-02-27T21_51_31-Measurement 5/Images"
 # input_dir="/fsx/processed-data/220929 Mattek 20x SD MIP TIFs/max_projections"
-output_dir="/lab/processed-data/EXP_JSM002_NGN2_DIV25_wholeplate/2023-02-15_cellpose_tdp-43"
+output_dir="/lab/processed-data/230227_iN_B3_DIV28_LiveCell/1__2023-02-27T21_51_31-Measurement 5/2023-02-28_soma_objects"
 plugins_dir="/home/ubuntu/CellProfiler-plugins"
-cppipe_path='/home/ubuntu/cell-imaging/cellprofiler_pipelines/2023-02-15_cellpose_tdp-43.cppipe'
+cppipe_path='/home/ubuntu/cell-imaging/cellprofiler_pipelines/2023-02-28_soma_objects.cppipe'
 
 #conda init /bin/bash
 #conda activate cell-imaging-2
