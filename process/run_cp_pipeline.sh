e64494426f104b206f8d92587945c2c32d9c1f3d
nmra-abarthmaron <asa.barthmaron@neumoratx.com>
2023-03-09 09:37:16 -0500

    GBA run

--- process/run_cp_pipeline.sh
+++ process/run_cp_pipeline.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-input_dir="/lab/raw-data/230227_iN_B3_DIV28_LiveCell/1__2023-02-27T21_51_31-Measurement 5/Images"
+input_dir="/lab/raw-data/20230223_GBA_EG_probetest_HeLa/20230223v3__2023-02-23T12_29_12-Measurement 1/Images"
 # input_dir="/fsx/processed-data/220929 Mattek 20x SD MIP TIFs/max_projections"
-output_dir="/lab/processed-data/230227_iN_B3_DIV28_LiveCell/1__2023-02-27T21_51_31-Measurement 5/2023-02-28_soma_objects"
+output_dir="/lab/processed-data/20230223_GBA_EG_probetest_HeLa/20230223v3__2023-02-23T12_29_12-Measurement 1/2023-03-08_GBA_analysis"
 plugins_dir="/home/ubuntu/CellProfiler-plugins"
-cppipe_path='/home/ubuntu/cell-imaging/cellprofiler_pipelines/2023-02-28_soma_objects.cppipe'
+cppipe_path='/home/ubuntu/cell-imaging/cellprofiler_pipelines/2023-03-08_GBA_analysis.cppipe'
 
 #conda init /bin/bash
 #conda activate cell-imaging-2
