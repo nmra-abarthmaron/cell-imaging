bd29707c940e18644aada128f9498200d8c0bf49
nmra-abarthmaron <asa.barthmaron@neumoratx.com>
2023-03-10 16:52:45 -0500

    analysis notebooks and cellpose function

--- utility/max_project.py
+++ utility/max_project.py
@@ -65,7 +65,82 @@ def max_project_PE(data_dir, overwrite_stacks=True):
                     os.remove(data_dir / file)
             else:
                 skimage.io.imsave(data_dir.parents[0] / 'MIPs' / z_section_files[0], mip)
-    
+
+def cellpose_stack(data_dir, cellpose_dir, primary_ch='ch3', secondary_ch=None):
+    """
+    Write images to a directory for cellpose training. If two channels are
+    specified they will be combined. Images will be tagged with experiment and
+    measurement names. Assumes 2D images.
+
+    Parameters
+    ----------
+    data_dir: pathlib.Path
+        Path to PE Export. Images must be 2D and not be stakcs or single 
+        z-slices where multiple z-slices of the same image are present. 
+    cellpose_dir : pathlib.Path
+        Path to save cellpose training images.
+    primary_ch: str, default = 'ch3'
+        Name of the primary channel to be segmented. Typically cytoplasm/soma 
+        or nucleus. 
+    secondary_ch: str, default = None
+        Name of the secondary channel to be segmented, if one is given.
+    """
+
+    # Find common field-of-view (fov) prefixes and channel numbers, using 
+    # standardized Harmony nomenclature. 
+    fov_prefixes = np.sort(np.unique([f.name[:9] for f in data_dir.glob('*.tiff')]))
+    channels = np.sort(np.unique([f.name[13:16] for f in data_dir.glob('*.tiff')]))
+
+    # Load test file to get dimesnions, would be nice to get this from xml
+    test_file = [f for f in data_dir.glob('*.tiff')][0]
+    tif = skimage.io.imread(test_file)
+    img_dims = tif.shape
+
+    # Add optional second channel if present
+    channels = [primary_ch]
+    if secondary_ch is not None:
+        channels.append(secondary_ch)
+
+    for prefix in fov_prefixes:
+
+        # Alllocate img array
+        img = np.zeros(
+            (len(channels), 
+            img_dims[0],
+            img_dims[1])
+        ).astype('uint16')
+
+        for i_ch, ch in enumerate(channels):
+
+            # Get filename for the fov prefix and channel. 'p01' defines first
+            # z-plane, 'sk1fk1fl1.tiff' is fixed and means nothing.
+            fname = prefix + 'p01' + ch + 'sk1fk1fl1.tiff'
+
+            # Load 
+            tif = skimage.io.imread(data_dir / fname)
+            img[i_ch, :, :] = tif
+        
+        # Make sure primary_ch is the channel name in saved file
+        fname = fname.replace(ch, primary_ch)
+
+        experiment = data_dir.parents[1].name
+        measurement = data_dir.parents[0].name
+        # Add experiment and measurement names to file name
+        fname = (
+            experiment + '_' + 
+            measurement + '_' + 
+            fname
+        )
+        # Save results
+        tifffile.imwrite(
+            cellpose_dir / experiment / measurement / fname,
+            img,
+            metadata={'axes': 'CYX'}, 
+            imagej=True,
+            compression=None,
+            dtype='uint16'
+        )
+
 def max_project(data_path, save_dir, overwrite=False):
     """
     Max projection function from earlier (pre 2023) experiments. 
